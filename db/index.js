1c7c723712b41d7a01368126860285096e33ed1c
siam <rifadul5@gmail.com>
2026-05-10 15:32:49 +0600

    refactor: update database connection to use DATABASE_URL and remove individual parameters

--- db/index.js
+++ db/index.js
@@ -1,12 +1,16 @@
 import 'dotenv/config'
 import pg from 'pg'
 
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+const { Pool } = pg
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+
+    ssl: isProduction
+        ? { rejectUnauthorized: false }
+        : false,
 })
 
 export default pool
