7972c5176a1580db4f2cf7fe744d07afd0295d4d
siam <rifadul5@gmail.com>
2026-05-10 16:24:14 +0600

    fix: update allowedOrigins for production environment

--- constants/allowedOrigins.js
+++ constants/allowedOrigins.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const allowedOrigins = isProduction
-    ? ['https://your-app-name.netlify.app', 'https://api.yourdomain.com']
+    ? ['https://node-api-postgres-zbuq.onrender.com']
     : ['http://localhost:3000', 'http://localhost:5173'];
