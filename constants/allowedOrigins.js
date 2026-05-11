26bbc6dfba14b99d565481d4a133bf353e845bfe
siam <rifadul5@gmail.com>
2026-05-11 10:30:26 +0600

    fix: update allowedOrigins to include additional local development environment

--- constants/allowedOrigins.js
+++ constants/allowedOrigins.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const allowedOrigins = isProduction
-    ? ['https://node-api-postgres-zbuq.onrender.com']
-    : ['http://localhost:3000', 'http://localhost:5173'];
+    ? ['https://node-api-postgres-zbuq.onrender.com', 'http://localhost:3000']
+    : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:3001'];
